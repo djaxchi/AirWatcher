8046c192a02df77a6bd403e348333cab30662be2
Xx-DarkBGdu69-xX <marcsauce02@gmail.com>
2024-05-16 08:48:35 +0200

    first push

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+                           Main  -  description
+                             -------------------
+    début                : 17/01/2023
+    copyright            : (C) 2023 par Nour ELJADIRI, Marie ROULIER
+    e-mail               : mohamed-nour.eljadiri@insa-lyon.fr
+                           marie.roulier@insa-lyon.fr
+*************************************************************************/
+
+//---------- Réalisation du module <Main> (fichier Main.cpp) ---------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//------------------------------------------------------ Include personnel
+#include "Analog.h"
+
+//-------------------------------------------------------- Include système
+using namespace std;
+
+///////////////////////////////////////////////////////////////////  PRIVE
+//------------------------------------------------------ Fonctions privées
+
+int main ( int argc, char * argv [ ] )
+// Algorithme :
+// Appel à la procédure de test
+{
+    return test ( argc, argv );
+} //----- fin de main
